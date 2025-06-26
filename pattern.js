90b8156b700f88791bc641b9744ec191b833af3d
Imran-Siddiquii <siddiquiimran132@gmail.com>
2025-06-26 08:33:41 +0530

    prob: pattern programming

--- pattern.js
+++ pattern.js
@@ -130,3 +130,62 @@ for (let i = 1; i <= n; i++) {
   }
   console.log('');
 }
+
+// Q8
+//  *       *
+//    *   *
+//      *
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n * 2 - 1; j++) {
+    if (i == j || i + j == n * 2) {
+      process.stdout.write('* ');
+    } else {
+      process.stdout.write('  ');
+    }
+  }
+  console.log('');
+}
+
+//  Q9
+//    *
+//  *   *
+//*       *
+//  *   *
+//    *
+//
+
+console.log('---------------------------------');
+
+for (let i = 1; i <= n * 2 - 1; i++) {
+  for (let j = 1; j <= n * 2 - 1; j++) {
+    if (
+      i + j == n + 1 ||
+      i - j == n - 1 ||
+      i + j == n * 3 - 1 ||
+      (i + j > n && Math.abs(i - j) == n - 1)
+    ) {
+      process.stdout.write('* ');
+    } else {
+      process.stdout.write('  ');
+    }
+  }
+
+  console.log('');
+}
+
+// not completed
+for (let i = 1; i <= n * 2 - 1; i++) {
+  for (let j = 1; j <= n * 2 - 1; j++) {
+    if (
+      (i + j >= n + 1 && i + j > n && Math.abs(i - j) <= n - 1) ||
+      (i - j == n - 1 && i + j >= n * 3 - 1)
+    ) {
+      process.stdout.write('* ');
+    } else {
+      process.stdout.write('  ');
+    }
+  }
+
+  console.log('');
+}
